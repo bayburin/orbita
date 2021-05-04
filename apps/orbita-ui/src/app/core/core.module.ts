3424f38eff5937d07f68e9c131865d2ab42c0225
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2021-05-04 17:44:40 +0700

    Добавлена библиотека angular2-actioncable

--- apps/orbita-ui/src/app/core/core.module.ts
+++ apps/orbita-ui/src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AuthCenterModule } from '@iss/ng-auth-center';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ActionCableService } from 'angular2-actioncable';
 
 import { environment } from '../../environments/environment';
 import { ORBITA_UI_ENV_TOKEN } from '@orbita/shared/environment';
@@ -20,7 +21,8 @@ import { JsonInterceptor } from './interceptors/json.interceptor';
   ],
   providers: [
     { provide: ORBITA_UI_ENV_TOKEN, useValue: environment },
-    { provide: HTTP_INTERCEPTORS, useClass: JsonInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: JsonInterceptor, multi: true },
+    ActionCableService
   ]
 })
 export class CoreModule {
