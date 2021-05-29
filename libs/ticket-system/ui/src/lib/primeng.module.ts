a1ec9d2df49ad13a88152d210ad30a718afd6dc5
Байбурин Равиль <baiburinrf@gmail.com>
2021-05-30 02:00:09 +0700

    Изменено отображение карточки события

--- libs/ticket-system/ui/src/lib/primeng.module.ts
+++ libs/ticket-system/ui/src/lib/primeng.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 import { TagModule } from 'primeng/tag';
+import { DividerModule } from 'primeng/divider';
+import { TooltipModule } from 'primeng/tooltip';
 
 const modules: any[] = [
   TableModule,
-  TagModule
+  ButtonModule,
+  CardModule,
+  TagModule,
+  DividerModule,
+  TooltipModule
 ];
 
 @NgModule({
