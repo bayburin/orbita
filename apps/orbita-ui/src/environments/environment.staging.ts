de374602a47cdd5ce93ea797f4b368210da57377
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2021-06-03 16:16:52 +0700

    Добавлена staging среда

---
+++ apps/orbita-ui/src/environments/environment.staging.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  serverApiUrl: 'https://orbita-center-test.iss-reshetnev.ru/api/v1',
+  actionCableUrl: 'wss://orbita-center-test.iss-reshetnev.ru/cable',
+  serviceDeskApi: 'https://sd-center.iss-reshetnev.ru/api',
+  svtApi: 'https://svt.iss-reshetnev.ru',
+  auth: {
+    clientId: '108',
+    redirectUrl: 'https://orbita-test.iss-reshetnev.ru/oauth2/callback',
+    serverUrl: 'https://orbita-center-test.iss-reshetnev.ru/api/v1/auth/token',
+    appName: 'Орбита (dev)',
+    jwtOptions: {
+      allowedDomains: ['orbita-center-test.iss-reshetnev.ru'],
+      disallowedRoutes: []
+    }
+  }
+};
