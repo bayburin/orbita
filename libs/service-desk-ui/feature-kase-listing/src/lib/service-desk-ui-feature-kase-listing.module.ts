f026dcc80f5f16428a8ac94833ca3a9a9cf52015
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2021-12-29 09:17:52 +0700

    Исправлены нерабочие ссылки на страницу создания заявки

--- libs/service-desk-ui/feature-kase-listing/src/lib/service-desk-ui-feature-kase-listing.module.ts
+++ libs/service-desk-ui/feature-kase-listing/src/lib/service-desk-ui-feature-kase-listing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ServiceDeskUiUiModule } from '@orbita/service-desk-ui/ui';
 import { ServiceDeskUiDomainLogicModule } from '@orbita/service-desk-ui/domain-logic';
@@ -6,7 +7,7 @@ import { ServiceDeskUiDomainLogicModule } from '@orbita/service-desk-ui/domain-l
 import { KaseListingComponent } from './kase-listing/kase-listing.component';
 
 @NgModule({
-  imports: [CommonModule, ServiceDeskUiUiModule, ServiceDeskUiDomainLogicModule],
+  imports: [CommonModule, RouterModule, ServiceDeskUiUiModule, ServiceDeskUiDomainLogicModule],
   declarations: [KaseListingComponent],
   exports: [KaseListingComponent],
 })
