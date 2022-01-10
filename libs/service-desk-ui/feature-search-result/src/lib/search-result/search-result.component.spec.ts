20ed875915f970de94106eaae1aa63052f406a6a
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2022-01-10 16:30:54 +0700

    Обновлен @ng-bootstrap/ng-bootstrap до версии 12

--- libs/service-desk-ui/feature-search-result/src/lib/search-result/search-result.component.spec.ts
+++ libs/service-desk-ui/feature-search-result/src/lib/search-result/search-result.component.spec.ts
@@ -1,9 +1,16 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import {
   DeepSearchFacade,
   DeepSearchFacadeStub,
   EmployeeFacade,
   EmployeeFacadeStub,
+  AttachmentFacade,
+  AttachmentFacadeStub,
+  QuestionFacade,
+  QuestionFacadeStub,
 } from '@orbita/service-desk-ui/domain-logic';
 
 import { SearchResultComponent } from './search-result.component';
@@ -14,11 +21,15 @@ describe('SearchResultComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [NgbModule, RouterTestingModule],
       declarations: [SearchResultComponent],
       providers: [
         { provide: DeepSearchFacade, useClass: DeepSearchFacadeStub },
         { provide: EmployeeFacade, useClass: EmployeeFacadeStub },
+        { provide: AttachmentFacade, useClass: AttachmentFacadeStub },
+        { provide: QuestionFacade, useClass: QuestionFacadeStub },
       ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
 
